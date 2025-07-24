7c60221a3df6859696a288319aac71a4fc4a885e
Joches29 <jochaveshe@est.utn.ac.cr>
2025-07-24 14:36:05 -0600

    Se modifica la clase tiempo
    Se cambia la clase tiempo para que vaya de lo general a lo especifico

--- src/Tiempo/Tiempo.java
+++ src/Tiempo/Tiempo.java
@@ -9,44 +9,22 @@ package Tiempo;
  * @author rodol
  */
 public class Tiempo {
-    private int hora;
-    private int minuto;
-    private int segundo;
+    private int contador;
 
-    public int getHora() {
-        return hora;
+    public int getContador(){
+        return contador;
     }
 
-    public int getMinuto() {
-        return minuto;
-    }
-
-    public int getSegundo() {
-        return segundo;
-    }
-
-    public void setHora(int hora) {
-        this.hora = hora;
-    }
-
-    public void setMinuto(int minuto) {
-        this.minuto = minuto;
-    }
-
-    public void setSegundo(int segundo) {
-        this.segundo = segundo;
+    public void setContador(int contador){
+        this.contador = contador;
     }
 
     public Tiempo() {
-        this.hora = 0;
-        this.minuto = 0;
-        this.segundo = 0;
+        this.contador = 0;
     }
     
     public void restart(){
-        this.hora = 0;
-        this.minuto = 0;
-        this.segundo = 0;
+        this.contador = 0;
     }
     
 }
