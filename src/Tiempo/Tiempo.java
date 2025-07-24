56735a9fd0c373f4d406c68991bb79168ed48ab4
Joches29 <jochaveshe@est.utn.ac.cr>
2025-07-24 15:46:21 -0600

    Modificacion de clase Tiempo
    Se modifica la clase tiempo para que este correctamente estructurado

--- src/Tiempo/Tiempo.java
+++ src/Tiempo/Tiempo.java
@@ -9,22 +9,71 @@ package Tiempo;
  * @author rodol
  */
 public class Tiempo {
-    private int contador;
+    private int hora;
+    private int minuto;
+    private int segundo;
 
-    public int getContador(){
-        return contador;
+    public int getHora() {
+        return hora;
     }
 
-    public void setContador(int contador){
-        this.contador = contador;
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
     }
 
     public Tiempo() {
-        this.contador = 0;
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
     }
     
-    public void restart(){
-        this.contador = 0;
+    public void correr(){
+        segundo++;
+        if (segundo == 60) {
+            segundo = 0;
+            minuto++;
+            if (minuto == 60) {
+                minuto = 0;
+                hora++;
+                if (hora == 24) {
+                    hora = 0;
+                }
+            }
+        }
     }
+
+    @Override
+    public String toString() {
+        String h,m,s;
+        h = String.valueOf(hora);
+        if (hora<10){
+            h="0".concat(h);
+        }
+        m = String.valueOf(minuto);
+        if (minuto < 10){
+            m="0".concat(m);
+        }
+        s = String.valueOf(segundo);
+        if (segundo<10){
+            s="0".concat(s);
+        }
+    }
+    
     
 }
