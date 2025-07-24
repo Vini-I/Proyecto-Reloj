eb71e97ea2926ef0384511a5db55bb5277b875d4
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2025-07-24 16:06:14 -0600

    Se crea la interfaz que permite configurar el reloj
    Esta interfaz permite configurar el reloj y el temporizador a la hora necesaria.

---
+++ src/Tiempo/Configurable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tiempo;
+
+/**
+ *
+ * @author autoa
+ */
+public interface Configurable {
+    public void configurar(int h, int m, int s);
+}
