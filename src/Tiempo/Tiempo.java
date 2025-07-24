add8a55fe1c8d1c63c6348dd12ff11ded8de862b
Joches29 <jochaveshe@est.utn.ac.cr>
2025-07-24 16:25:42 -0600

    Modificacion de clase Tiempo
    Se modifica la clase tiempo eliminando metodos de acceso y agregando la variable activo

--- src/Tiempo/Tiempo.java
+++ src/Tiempo/Tiempo.java
@@ -12,35 +12,13 @@ public class Tiempo {
     protected int hora;
     protected int minuto;
     protected int segundo;
-
-    public int getHora() {
-        return hora;
-    }
-
-    public int getMinuto() {
-        return minuto;
-    }
-
-    public int getSegundo() {
-        return segundo;
-    }
-
-    public void setHora(int hora) {
-        this.hora = hora;
-    }
-
-    public void setMinuto(int minuto) {
-        this.minuto = minuto;
-    }
-
-    public void setSegundo(int segundo) {
-        this.segundo = segundo;
-    }
+    protected boolean activo;
 
     public Tiempo() {
         this.hora = 0;
         this.minuto = 0;
         this.segundo = 0;
+        this.activo = false;
     }
     
     public void correr(){
